0c15295fe315cf2440277522fd0e8de6232cc01a
Diovane Alves <alvdiovane@gmail.com>
2023-04-15 18:18:47 -0300

    🎉Recreating project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useEffect } from "react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import { About } from "./pages/About";
-import { Contact } from "./pages/Contact";
-import { Footer } from "./pages/Footer";
-import { Home } from "./pages/Home";
-import { Project } from "./pages/Project";
-import { Skills } from "./pages/Skills";
-
-import AOS from "aos";
-import "./styles/main.css";
-
-export function App() {
-  useEffect(() => {
-    AOS.init({
-      duration: 400,
-      anchorPlacement: "top-center",
-      once: true,
-      mirror: true,
-      easing: "ease-in",
-    });
-  }),
-    [];
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="scroll-smooth">
-      <Home />
-      <About />
-      <Project />
-      <Skills />
-      <Contact />
-      <Footer />
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
-  );
+  )
 }
+
+export default App
