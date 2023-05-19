8c1ea514cfcb81a21af1bb60270a00a415dc1c86
Diovane Alves <alvdiovane@gmail.com>
2023-05-18 22:03:48 -0300

    Updating the form for FORMSPREE

--- src/components/FormContact/index.tsx
+++ src/components/FormContact/index.tsx
@@ -1,82 +1,54 @@
-import { useState, FormEvent } from "react";
-import emailjs from "@emailjs/browser";
+import { useForm, ValidationError } from "@formspree/react";
 import SuccessfulMailing from "../SuccessfulMailing";
 
 export function FormContact() {
-  const [state, setState] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [state, handleSubmit] = useForm(import.meta.env.VITE_FORMSPREE_KEY);
 
-  function sendEmail(e: FormEvent) {
-    e.preventDefault();
-    const templateParams = {
-      from_name: name,
-      email: email,
-      message: message,
-    };
-
-    if (name === "" || email === " " || message === "") {
-      return alert("Preencha todos os campos");
+  if (state.succeeded) {
+    for (const form of document.getElementsByTagName("form")) {
+      form.reset();
     }
-
-    emailjs
-      .send(
-        import.meta.env.VITE_EMAILJS_SERVICEID,
-        import.meta.env.VITE_EMAILJS_TEMPLATEID,
-        templateParams,
-        import.meta.env.VITE_EMAILJS_PUBLICKEY
-      )
-      .then(
-        (res) => {
-          setState(true);
-          setName("");
-          setEmail("");
-          setMessage("");
-        },
-        (err) => {
-          console.log("error");
-        }
-      );
   }
 
   return (
-    <form className="flex flex-col gap-4 mt-10" onSubmit={sendEmail}>
-      <input
-        className="bg-[#041A27] rounded-lg border-2 border-[#8CD1FF] text-center p-1"
-        pattern="^[a-zA-Z]{2,}(?: [a-zA-Z]+){1,}$"
-        title="Por favor, insira seu nome completo"
-        type="text"
-        placeholder="Digite seu nome"
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-      />
+    <form className="flex flex-col gap-4 mt-10" onSubmit={handleSubmit}>
+      <input name="subject" type="hidden" value="Formulario do Portfolio" />
 
       <input
         className="bg-[#041A27] rounded-lg border-2 border-[#8CD1FF] text-center p-1"
-        pattern="^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$"
-        title="Por favor, insira um endereço de email válido."
         type="text"
-        placeholder="Digite seu email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
+        id="name"
+        name="name"
+        placeholder="Digite seu nome"
       />
+      <ValidationError field="name" prefix="Name" errors={state.errors} />
+
+      <input
+        className="bg-[#041A27] rounded-lg border-2 border-[#8CD1FF] text-center p-1"
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Digite seu email"
+      />
+      <ValidationError field="email" prefix="Email" errors={state.errors} />
 
       <textarea
         className="bg-[#041A27] rounded-lg border-2 border-[#8CD1FF] h-40 p-2 resize-none text-center"
         placeholder="Digite sua mensagem..."
-        onChange={(e) => setMessage(e.target.value)}
-        value={message}
+        id="message"
+        name="message"
       />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
 
       <button
         className="bg-[#8CD1FF] text-black font-bold rounded-lg border-2 border-[#8CD1FF] h-12 hover:bg-[#041A27] hover:text-[#8CD1FF]"
         type="submit"
+        disabled={state.submitting}
       >
         Enviar
       </button>
 
-      {state && <SuccessfulMailing />}
+      {state.succeeded && <SuccessfulMailing />}
     </form>
   );
 }
