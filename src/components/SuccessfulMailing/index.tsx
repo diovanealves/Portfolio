05cac7bf73fe3d6c77326d645e447bd94c5c0e1c
Diovane Alves <alvdiovane@gmail.com>
2023-05-18 23:13:09 -0300

    fixing the url of the video

--- src/components/SuccessfulMailing/index.tsx
+++ src/components/SuccessfulMailing/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import VideoEmailSent from "../../assets/successfullySend.mp4";
 
 export default function SuccessfulMailing() {
   const [visible, setVisible] = useState(true);
@@ -16,12 +17,7 @@ export default function SuccessfulMailing() {
   return (
     <div className="fixed inset-0 z-50 bg-[#041A27] text-black flex justify-center items-center p-8">
       <div className="bg-white py-16 px-8 rounded-3xl">
-        <video
-          src="./src/assets/successfullySend.mp4"
-          muted
-          autoPlay
-          className="w-64 inline"
-        />
+        <video src={VideoEmailSent} muted autoPlay className="w-64 inline" />
         <h1 className="text-4xl font-bold">
           Menssagem enviada <br /> com sucesso!
         </h1>
