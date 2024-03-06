30c71a929726c4179223bef59b1ad32f5e01bdde
Diovane Alves <alvdiovane@gmail.com>
2024-03-05 21:13:55 -0300

    feat:  Adds language configuration using i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "pt-br"],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
 });
