39e6aa51e44b0d650b490a98036bdcb87acb41f4
Diovane Alves <alvdiovane@gmail.com>
2024-03-05 23:17:39 -0300

    feat: Implement toggle functionality for light and dark themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "pt-br"],
     routing: {
-      prefixDefaultLocale: true,
-    },
-  },
+      prefixDefaultLocale: true
+    }
+  }
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.astro"],
+  content: ["./src/**/*.{astro, tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundDark: "#052131",
+        textDark: "#DFF6FF",
+      },
+    },
   },
   plugins: [],
 };
