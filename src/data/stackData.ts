9e1a6396972a908150eb96d2102e6fb14b54360b
Diovane Alves <alvdiovane@gmail.com>
2024-03-17 22:49:47 -0300

    refactor: Add official page links for front-end technology and fix icon color for Radix and NextJS

--- src/data/stackData.ts
+++ src/data/stackData.ts
@@ -3,6 +3,8 @@ export interface stackProps {
   [key: string]: {
     image: string;
     title: string;
+    href: string;
+    classname?: string;
   }[];
 }
 
@@ -11,17 +13,52 @@ export const stackData: stackProps = {
     {
       image: icon.html.src,
       title: "HTML",
+      href: "https://developer.mozilla.org/pt-BR/docs/Web/HTML",
     },
-    { image: icon.css.src, title: "CSS" },
-    { image: icon.javascript.src, title: "JavaScript" },
-    { image: icon.typescript.src, title: "TypeScript" },
-    { image: icon.react.src, title: "React" },
-    { image: icon.framerMotion.src, title: "Framer Motion" },
-    { image: icon.tailwindCss.src, title: "Tailwind CSS" },
-    { image: icon.sass.src, title: "SASS" },
-    { image: icon.radixUi.src, title: "Radix UI" },
-    { image: icon.nextjs.src, title: "Next.js" },
-    { image: icon.tanstackQuery.src, title: "Tanstack Query" },
-    { image: icon.vitejs.src, title: "Vite.js" },
+    {
+      image: icon.css.src,
+      title: "CSS",
+      href: "https://developer.mozilla.org/pt-BR/docs/Web/CSS",
+    },
+    {
+      image: icon.javascript.src,
+      title: "JavaScript",
+      href: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript",
+    },
+    {
+      image: icon.typescript.src,
+      title: "TypeScript",
+      href: "https://www.typescriptlang.org/",
+    },
+    { image: icon.react.src, title: "React", href: "https://react.dev/" },
+    {
+      image: icon.framerMotion.src,
+      title: "Framer Motion",
+      href: "https://www.framer.com/motion/",
+    },
+    {
+      image: icon.tailwindCss.src,
+      title: "Tailwind CSS",
+      href: "https://tailwindcss.com/",
+    },
+    { image: icon.sass.src, title: "SASS", href: "https://sass-lang.com/" },
+    {
+      image: icon.radixUi.src,
+      title: "Radix UI",
+      href: "https://www.radix-ui.com/",
+      classname: "dark:invert",
+    },
+    {
+      image: icon.nextjs.src,
+      title: "Next.js",
+      href: "https://nextjs.org/",
+      classname: "dark:invert",
+    },
+    {
+      image: icon.tanstackQuery.src,
+      title: "Tanstack Query",
+      href: "https://tanstack.com/query/latest",
+    },
+    { image: icon.vitejs.src, title: "Vite.js", href: "https://vitejs.dev/" },
   ],
 };
