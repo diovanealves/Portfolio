db9415a6b56deecfe18f8efb03f9ff96399722b7
Diovane Alves <alvdiovane@gmail.com>
2024-03-18 21:04:08 -0300

    feat: Integrate astro-compress for code optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    (await import("astro-compress")).default(),
+  ],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "pt-br"],
     routing: {
-      prefixDefaultLocale: true
-    }
-  }
+      prefixDefaultLocale: true,
+    },
+  },
 });
