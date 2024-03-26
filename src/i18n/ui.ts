d30c613832b3a6a123497f7348cbb947b2aa0bd0
Diovane Alves <alvdiovane@gmail.com>
2024-03-25 23:07:08 -0300

    feat: Start configuring i18n for Command component translation

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+  en: "English",
+  "pt-br": "Português (Brasil)",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "command.input": "Type a command or search...",
+    "command.empty": "No results found",
+    "command.home": "Home",
+    "command.project": "My Projects",
+    "command.stack": "Technologies and Tools",
+    "command.e-mail": "Send E-mail",
+  },
+  "pt-br": {
+    "command.input": "Digite um comando ou pesquise...",
+    "command.empty": "Nenhum resultado encontrado",
+    "command.home": "Home",
+    "command.project": "Meus Projetos",
+    "command.stack": "Tecnologias e Ferramentas",
+    "command.e-mail": "Enviar E-mail",
+  },
+} as const;
