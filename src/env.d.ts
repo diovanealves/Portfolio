6dfc2fd5cfe2dbaf3ebcc7a99efb29db20229a61
Diovane Alves <alvdiovane@gmail.com>
2024-03-27 22:46:55 -0300

    feat: Create projects interface and configure .env import for Hygraph

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly HYGRAPH_API: string;
+}
---
+++ src/types/project.interface.ts
@@ -0,0 +1,8 @@
+export interface Project {
+  title: string;
+  description: string;
+  usedLanguages: string[];
+  projectWebsite: string;
+  linkImage: string;
+  githubRepository: string;
+}
